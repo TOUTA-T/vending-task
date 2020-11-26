e7992d2e60bb3a736c347506bbe07d7549ca6076
sincrare <sincrare@sonicgarden.jp>
2020-11-26 16:53:55 +0900

    SGコードレビュー20201126

--- vending.rb
+++ vending.rb
@@ -1,102 +1,123 @@
 class VendingMachine
-  attr_reader :items, :slot_money, :sales_money
-​
-  def initialize(items:)
-    @items = items
+  attr_reader :sales_money, :drink_stock
+
+  def initialize()
+    @drink_stock = DrinkStock.new
+    # @items = items
     @slot_money = 0
     @sales_money = 0
-    @reject_money = 0
   end
-​
+
+  def items
+    @drink_stock.items
+  end
+
   MONEY = [10, 50, 100, 500, 1000].freeze
-​
+
   def slot_money(money)
     unless MONEY.include?(money)
       puts "お取り扱いできません"
-      @reject_money += money
-      return return_money
+      return display_return_money(money)
     end
     @slot_money += money
     puts "投入金額:#{@slot_money}円"
   end
-​
+
   def current_slot_money
     @slot_money
   end
-​
-  def return_money
-    if @reject_money != 0
-      puts "お釣りは #{@reject_money}円です"
-      @reject_money = 0
-    else
-      puts "お釣りは #{@slot_money}円です"
-      @slot_money = 0
-    end
+
+  def add_drink
+    @drink_stock.add_item(xxx)
   end
-​
+
+  # def return_money
+  #   if @reject_money != 0
+  #     puts "お釣りは #{@reject_money}円です"
+  #     @reject_money = 0
+  #   else
+  #     puts "お釣りは #{@slot_money}円です"
+  #     @slot_money = 0
+  #   end
+  # end
+
+  def display_return_money(money)
+    puts "お釣りは #{money}円です"
+  end
+
   def sales
     puts "売り上げ: #{@sales_money}円"
   end
-​
-  def menu
+
+  # 動作を表現したい時は動詞+名詞のほうがわかりやすいかも
+  def display_menu
     puts "メニューはこちらです"
     puts "*" * 50
-    @items.each_with_index do |item, i|
-      puts "#{i}: #{item[:name]} (#{item[:price]}円)" if judge(i)
+    @drink_stock.item.each_with_index do |item, i|
+      puts "#{i}: #{item[:name]} (#{item[:price]}円)" if buyable?(i)
     end
     puts "*" * 50
   end
-​
+
   def purchase(drink_number)
-    if judge(drink_number)
+    if buyable?(drink_number)
       puts "#{items[drink_number][:name]} を購入しました!"
       items[drink_number][:stock] -= 1
       @sales_money += items[drink_number][:price]
-      @slot_money = current_slot_money - items[drink_number][:price]
-      return_money
+      # @slot_money = current_slot_money - items[drink_number][:price]
+      @slot_money -= items[drink_number][:price]
+      display_return_money(@slot_money)
+      @slot_money = 0
     end
   end
-​
-  def judge(drink_number)
-    items[drink_number][:price] <= current_slot_money && items[drink_number][:stock] > 0
+
+  private
+
+  # true falseを返すメソッドには?をつけるのがrubyだとよくやる
+  def buyable?(drink_number)
+    items[drink_number][:price] <= @slot_money && items[drink_number][:stock] > 0
   end
-​
+
   def item_info
     items.each do |item|
       puts "名前: #{item[:name]}  値段: #{item[:price]}  在庫: #{item[:stock]}個"
     end
   end
 end
-​
-​
-class Drink
-  attr_reader :item
-​
+
+class DrinkStock
+  attr_reader :items
+
   def initialize
-    @item = [{name: "コーラ", price: 120, stock: 5}]
+    # 変数に型がないので、単数複数形をちゃんと表現したほうが、中身がわかりやすい
+    @items = [{name: "コーラ", price: 120, stock: 5}]
   end
-​
+
   def add_item(name, price)
-    @item.push({name: name, price: price, stock: 5})
+    @items.push({name: name, price: price, stock: 5})
   end
-​
+
   def item_reset
-    @item = []
+    @items = []
   end
-​
+
   def current_item
-    @item
+    @items
   end
-​
+
   def item_info
-    @item.each do |item|
+    @items.each do |item|
       puts "名前: #{item[:name]}  値段: #{item[:price]}  在庫: #{item[:stock]}個"
     end
   end
 end
-​​
+
+class Drink
+  attr_reader :name, :price
+end
+
 # require "./vending"
-# drink = Drink.new
+# drink = DrinkStock.new
 # drink.add_item("水", 100)
 # drink.add_item("レッドブル", 200)
 # items = drink.current_item
@@ -112,3 +133,4 @@ end
 # vm.return_money
 # vm.sales
 # vm.item_info
+# vm.drink_stock.add_item(xxx)
